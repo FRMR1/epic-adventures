a4d169a9fdcf6f75569a8bea45a844cb0cd0f4b0
Wade Hunter <wade.hunter87@gmail.com>
2021-03-19 15:08:19 -0600

    Tawk.to live chat added

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,11 +1,21 @@
+import { useEffect } from "react"
 import Nav from "../Nav"
 import Footer from "../Footer"
+import tawkTo from "tawkto-react"
 import GlobalStyles from "../styles/GlobalStyles"
 
 const Layout = ({ children }) => {
     // Get current year
     const date = new Date()
     const year = date.getFullYear()
+
+    // Live chat
+    useEffect(() => {
+        tawkTo(
+            process.env.NEXT_PUBLIC_TAWK_TO_ID,
+            process.env.NEXT_PUBLIC_TAWK_TO_KEY
+        )
+    }, [])
 
     return (
         <div>
