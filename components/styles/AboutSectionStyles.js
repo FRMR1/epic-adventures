912c5662f3746c782c1a5319cc3e20dc1f74603c
Wade Hunter <wade.hunter87@gmail.com>
2022-03-29 14:20:07 -0600

    About page logos

--- components/styles/AboutSectionStyles.js
+++ components/styles/AboutSectionStyles.js
@@ -1,112 +1,168 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const AboutSectionStyles = styled.div`
-    background-color: ${({ grey }) => (grey ? "#f3f3f3" : "#fafafa")};
-    position: relative;
-    z-index: -1;
-    margin-top: 0;
-    padding: 8rem 3rem 0;
-    margin-bottom: ${({ bottom }) => (bottom ? "-50rem" : "0")};
-    padding-bottom: ${({ bottom }) => (bottom ? "50rem" : "0")};
+  background-color: ${({ grey }) => (grey ? "#f3f3f3" : "#fafafa")};
+  position: relative;
+  z-index: -1;
+  margin-top: 0;
+  padding: 8rem 3rem 0;
+  margin-bottom: ${({ bottom }) => (bottom ? "-50rem" : "0")};
+  padding-bottom: ${({ bottom }) => (bottom ? "50rem" : "0")};
+  @media (max-width: 800px) {
+    display: flex;
+    flex-wrap: wrap;
+    text-align: center;
+    padding-top: 17rem;
+  }
+  h3 {
+    margin: 0;
+    font-size: 5rem;
+    margin-top: 10rem;
+    color: var(--mainColor);
+    letter-spacing: -0.1rem;
+    text-align: center;
     @media (max-width: 800px) {
-        display: flex;
-        flex-wrap: wrap;
-        text-align: center;
-        padding-top: 17rem;
+      font-size: 3.5rem;
+      margin-bottom: 5rem;
+      line-height: 4rem;
+      text-align: center;
+    }
+  }
+  p {
+    max-width: 70rem;
+    margin: 1rem auto;
+    font-size: 1.7rem;
+    color: #b2b2b2;
+    @media (max-width: 800px) {
+      font-size: 1.5rem;
+    }
+  }
+  .bold {
+    font-weight: 600;
+  }
+  .small {
+    font-size: 1.5rem;
+  }
+  .big {
+    font-size: 2rem;
+    text-align: center;
+  }
+  .twoCol {
+    margin-top: 3rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    @media (max-width: 800px) {
+      display: flex;
+      flex-wrap: wrap;
+      text-align: center;
+      flex-direction: column-reverse;
     }
     h3 {
-        margin: 0;
-        font-size: 5rem;
-        margin-top: 10rem;
-        color: var(--mainColor);
-        letter-spacing: -0.1rem;
+      font-size: 4rem;
+      text-align: left;
+      @media (max-width: 800px) {
         text-align: center;
-        @media (max-width: 800px) {
-            font-size: 3.5rem;
-            margin-bottom: 5rem;
-            line-height: 4rem;
-            text-align: center;
-        }
+      }
     }
-    p {
-        max-width: 70rem;
-        margin: 1rem auto;
-        font-size: 1.7rem;
-        color: #b2b2b2;
-        @media (max-width: 800px) {
-            font-size: 1.5rem;
-        }
-    }
-    .bold {
-        font-weight: 600;
-    }
-    .small {
+    ul {
+      list-style: none;
+      text-align: left;
+      padding-left: 0;
+      li {
         font-size: 1.5rem;
-    }
-    .big {
-        font-size: 2rem;
-        text-align: center;
-    }
-    .twoCol {
-        margin-top: 3rem;
+        color: #7b7b7b;
+        margin-bottom: 2rem;
         display: grid;
-        grid-template-columns: 1fr 1fr;
-        @media (max-width: 800px) {
-            display: flex;
-            flex-wrap: wrap;
-            text-align: center;
-            flex-direction: column-reverse;
+        grid-template-columns: auto auto;
+        line-height: 3.3rem;
+        img {
+          margin-right: 1.6rem;
+          position: relative;
+          top: 1rem;
         }
-        h3 {
-            font-size: 4rem;
-            text-align: left;
-            @media (max-width: 800px) {
-                text-align: center;
-            }
-        }
-        ul {
-            list-style: none;
-            text-align: left;
-            padding-left: 0;
-            li {
-                font-size: 1.5rem;
-                color: #7b7b7b;
-                margin-bottom: 2rem;
-                display: grid;
-                grid-template-columns: auto auto;
-                line-height: 3.3rem;
-                img {
-                    margin-right: 1.6rem;
-                    position: relative;
-                    top: 1rem;
-                }
-            }
-        }
-        .left {
-            padding: 0 7rem;
-        }
-        .right {
-            padding: 0 7rem;
-            @media (max-width: 800px) {
-                width: 100%;
-            }
-            h3 {
-                color: #6a6a6a;
-            }
-        }
-        .imageRotate {
-            transform: rotate(5deg);
-            /* img {
+      }
+    }
+    .left {
+      padding: 0 7rem;
+    }
+    .right {
+      padding: 0 7rem;
+      @media (max-width: 800px) {
+        width: 100%;
+      }
+      h3 {
+        color: #6a6a6a;
+      }
+    }
+    .imageRotate {
+      transform: rotate(5deg);
+      /* img {
                 filter: drop-shadow(30px 10px 4px #4444dd);
             } */
-        }
-
-        .left,
-        .right {
-            align-self: center;
-            @media (max-width: 800px) {
-                padding: 0;
-            }
-        }
     }
-`
+
+    .left,
+    .right {
+      align-self: center;
+      @media (max-width: 800px) {
+        padding: 0;
+      }
+    }
+  }
+  .whyEpic {
+    margin-top: 18rem;
+    margin-bottom: 15rem;
+    h3 {
+      margin-bottom: 3rem;
+    }
+    .container {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: 1fr 1fr;
+      max-width: 80rem;
+      margin: 0 auto;
+      column-gap: 2rem;
+      row-gap: 4rem;
+      .item {
+        display: grid;
+        grid-template-columns: 20px 1fr;
+        column-gap: 2rem;
+      }
+      .icon {
+        justify-self: start;
+      }
+      .desc {
+        h4 {
+          text-align: left;
+          color: #505050;
+          margin: 0;
+          font-size: 3rem;
+          line-height: 3rem;
+        }
+        p {
+          text-align: left;
+          font-size: 1.5rem;
+          line-height: 3rem;
+        }
+      }
+    }
+  }
+  .logos {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .grid {
+      margin-top: 50px;
+      max-width: 1000px;
+      display: flex;
+      flex-wrap: wrap;
+      gap: 40px;
+      justify-content: center;
+      align-items: center;
+      img {
+        width: 200px;
+      }
+    }
+  }
+`;
--- pages/about.js
+++ pages/about.js
@@ -1,131 +1,155 @@
-import Image from "next/image"
-import Head from "next/head"
-import ReadyToBook from "../components/ReadyToBook"
-import Divider from "../components/Divider"
-import { HeroImageStyles } from "../components/styles/HeroImageStyles"
-import { SectionStyles } from "../components/styles/SectionStyles"
-import { TopDividerStyles } from "../components/styles/TopDividerStyles"
-import { BottomDividerStyles } from "../components/styles/BottomDividerStyles"
-import { AboutSectionStyles } from "../components/styles/AboutSectionStyles"
-import { FooterDividerStyles } from "../components/styles/FooterDividerStyles"
+import Image from "next/image";
+import Head from "next/head";
+import ReadyToBook from "../components/ReadyToBook";
+import Divider from "../components/Divider";
+import { HeroImageStyles } from "../components/styles/HeroImageStyles";
+import { SectionStyles } from "../components/styles/SectionStyles";
+import { TopDividerStyles } from "../components/styles/TopDividerStyles";
+import { BottomDividerStyles } from "../components/styles/BottomDividerStyles";
+import { AboutSectionStyles } from "../components/styles/AboutSectionStyles";
+import { FooterDividerStyles } from "../components/styles/FooterDividerStyles";
 
 const About = () => {
-    return (
-        <>
-            <Head>
-                <title>Epic Adventures | About</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <HeroImageStyles image="combo1">
-                <div className="heroText">
-                    <h2>We Are Epic.</h2>
-                </div>
-            </HeroImageStyles>
-            <TopDividerStyles>
-                <img
-                    src="/svg/divider-top.svg"
-                    width="101%"
-                    style={{ marginLeft: "-.1rem" }}
-                />
-            </TopDividerStyles>
-            <SectionStyles>
+  return (
+    <>
+      <Head>
+        <title>Epic Adventures | About</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <HeroImageStyles image="combo1">
+        <div className="heroText">
+          <h2>We Are Epic.</h2>
+        </div>
+      </HeroImageStyles>
+      <TopDividerStyles>
+        <img
+          src="/svg/divider-top.svg"
+          width="101%"
+          style={{ marginLeft: "-.1rem" }}
+        />
+      </TopDividerStyles>
+      <SectionStyles>
+        <p>
+          With a focus on Adventure Tourism and the Canadian Rockies, our
+          passion is the great outdoors. We work with the some of Canada's
+          Premiere tour operators to give you the best quality products and
+          authentic experiences throughout Canadas wild west. From Canmore,
+          Banff, Jasper, Lake Louise and Golden our mission is to give you the
+          best to save you time and money while you experience this playground
+          with us and make it Epic.
+        </p>
+      </SectionStyles>
+      <BottomDividerStyles>
+        <div className="inner">
+          <img
+            src="/svg/divider-bottom.svg"
+            width="101%"
+            style={{ marginLeft: "-.1rem" }}
+          />
+        </div>
+      </BottomDividerStyles>
+      <AboutSectionStyles>
+        <div className="whyEpic">
+          <h3>Why Epic?</h3>
+          <div className="container">
+            <div className="item">
+              <div className="icon">
+                <img src="/svg/checkmark.svg" width="25" />
+              </div>
+              <div className="desc">
+                <h4>Highest Standards</h4>
                 <p>
-                    With a focus on Adventure Tourism and the Canadian Rockies,
-                    our passion is the great outdoors. We work with the some of
-                    Canada's Premiere tour operators to give you the best
-                    quality products and authentic experiences throughout
-                    Canadas wild west. From Canmore, Banff, Jasper, Lake Louise
-                    and Golden our mission is to give you the best to save you
-                    time and money while you experience this playground with us
-                    and make it Epic.
+                  All of our operators have the years of experience required to
+                  give you the best quality trip and highest industry standards.
+                  They know their stuff.
                 </p>
-            </SectionStyles>
-            <BottomDividerStyles>
-                <div className="inner">
-                    <img
-                        src="/svg/divider-bottom.svg"
-                        width="101%"
-                        style={{ marginLeft: "-.1rem" }}
-                    />
-                </div>
-            </BottomDividerStyles>
-            <AboutSectionStyles>
-                <h3>Why Epic?</h3>
-                <div>
-                    <ul>
-                        <li>Quality</li>
-                        <li>Value</li>
-                        <li>Tested</li>
-                        <li>Authentically Canadian</li>
-                        <li>Experience More</li>
-                    </ul>
-                </div>
-                {/* <div className="twoCol">
-                    <div className="left">
-                        <p className="small">
-                            Epic Adventure Group is operated out of Golden,
-                            British Columbia by humans who want you to
-                            experience more out of Canada’s Adventure Capital.
-                            We allow you to make combination packages of the
-                            best adventures Golden has to offer, giving you
-                            exclusive savings on experiences you’ll never
-                            forget.
-                        </p>
-                    </div>
-                    <div className="right imageRotate">
-                        <Image
-                            src="/images/combo9.jpg"
-                            layout="responsive"
-                            width="100%"
-                            height="auto"
-                            objectFit="contain"
-                            alt="Epic Adventures"
-                        />
-                    </div>
-                </div> */}
-            </AboutSectionStyles>
-            <AboutSectionStyles grey={true} bottom={true}>
-                <Divider />
-                <div className="bottomSection">
-                    <h3 className="center">About Golden</h3>
-                    <p className="center">
-                        Golden is at the heart of the Canadian Rockies, where
-                        the Kicking Horse River and the Columbia River meet and
-                        offers access to some of the most pristine backcountry
-                        around. Surrounded by the vast mountain ranges of six
-                        incredible Canadian national parks, Golden is as
-                        picturesque as it is adventurous.
-                    </p>
-                    <p className="center">
-                        Home of the Ktunaxa people, other indigenous peoples
-                        also harvested, hunted, fished and settled seasonally
-                        within the area, including the Shuswap peoples who for
-                        centuries have travelled to and inhabited the Ktunaxa
-                        homelands. It was the spirit of adventure that brought
-                        the first explorers over the Rocky Mountains in the 19
-                        th century, and Golden still holds that same feeling of
-                        discovery and exploration for visitors and the locals
-                        who call this valley home.
-                    </p>
-                    <p className="center">
-                        Golden is located in southeastern British Columbia right
-                        on the Trans-Canada Highway, approximately 262
-                        kilometers west of Calgary or 713 kilometres east of
-                        Vancouver. Banff is a short 1 ½ hours or 4 hours to BC’s
-                        Okanagan Valley.
-                    </p>
-                </div>
-            </AboutSectionStyles>
-            <ReadyToBook />
-            <FooterDividerStyles>
-                <img
-                    src="/svg/divider-top2.svg"
-                    width="101%"
-                    style={{ marginLeft: "-.1rem" }}
-                />
-            </FooterDividerStyles>
-        </>
-    )
-}
+              </div>
+            </div>
+            <div className="item">
+              <div className="icon">
+                <img src="/svg/checkmark.svg" width="25" />
+              </div>
+              <div className="desc">
+                <h4>Hype Deals</h4>
+                <p>
+                  With every purchase you get access to local deals and offers
+                  on partnered hotels, restaurants and activities to save you
+                  money and connect you with local legends.
+                </p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="icon">
+                <img src="/svg/checkmark.svg" width="25" />
+              </div>
+              <div className="desc">
+                <h4>Authentically Canadian</h4>
+                <p>
+                  All of our adventures are operated and owned by local people
+                  and families. Your money stays local and that matters.
+                </p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="icon">
+                <img src="/svg/checkmark.svg" width="25" />
+              </div>
+              <div className="desc">
+                <h4>Best Value</h4>
+                <p>
+                  We hand pick the best value trips to save you time and
+                  research. Chosen by internationally experienced guides.
+                </p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="icon">
+                <img src="/svg/checkmark.svg" width="25" />
+              </div>
+              <div className="desc">
+                <h4>Experience More</h4>
+                <p>
+                  Our exclusive combos and adventures will save you money, time
+                  and research and let you experience more on your trip through
+                  the Rockies.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </AboutSectionStyles>
+      <AboutSectionStyles grey={true} bottom={true}>
+        <Divider />
+        <div className="logos">
+          <h3 className="center">Our Epic Family</h3>
+          <div className="grid">
+            <img src="/images/logos/alpine-dark.svg" />
+            <img src="/images/logos/banffgondola-dark.svg" />
+            <img src="/images/logos/chinookrafting.svg" />
+            <img src="/images/logos/coa.svg" />
+            <img src="/images/logos/columbiaicefield-dark.svg" />
+            <img src="/images/logos/extremeyeti-dark.svg" />
+            <img src="/images/logos/glacierrafting-dark.svg" />
+            <img src="/images/logos/jasperskytram-dark.svg" />
+            <img src="/images/logos/kickinghorse-dark.svg" />
+            <img src="/images/logos/malignelake-dark.svg" />
+            <img src="/images/logos/minnewanka-dark.svg" />
+            <img src="/images/logos/norquay-dark.svg" />
+            <img src="/images/logos/scenicflights-dark.svg" />
+            <img src="/images/logos/skybridge-dark.svg" />
+          </div>
+        </div>
+      </AboutSectionStyles>
+      <ReadyToBook />
+      <FooterDividerStyles>
+        <img
+          src="/svg/divider-top2.svg"
+          width="101%"
+          style={{ marginLeft: "-.1rem" }}
+        />
+      </FooterDividerStyles>
+    </>
+  );
+};
 
-export default About
+export default About;
