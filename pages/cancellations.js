8d8f2e2263e387b4f062bad52c5ba8f49c2f39d6
Wade Hunter <wade.hunter87@gmail.com>
2022-04-21 15:16:43 -0600

    Policy updates

--- pages/cancellations.js
+++ pages/cancellations.js
@@ -1,58 +1,55 @@
-import Head from "next/head"
-import { PolicyStyles } from "../components/styles/PolicyStyles"
-import { FooterDividerStyles } from "../components/styles/FooterDividerStyles"
+import Head from "next/head";
+import { PolicyStyles } from "../components/styles/PolicyStyles";
+import { FooterDividerStyles } from "../components/styles/FooterDividerStyles";
 
 const Cancellations = () => {
-    return (
-        <>
-            <Head>
-                <title>Epic Adventures | Cancellation Policy</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <PolicyStyles>
-                <h3>Cancellation Policy</h3>
-                <p>
-                    Purchasers have until 48-hours prior to their scheduled
-                    activity or tour to cancel and request a full refund, unless
-                    otherwise stated on the confirmation document. Just call us
-                    at least 48 hours prior to your tour to cancel and receive a
-                    full refund, no questions asked. Purchasers of combos or
-                    packages have until 72-hours prior to the first scheduled
-                    activity to cancel and request a refund. Any cancellations
-                    or requests received within 48 hours of your activity or
-                    tour, late-comers, and no- shows will not be rescheduled or
-                    refunded.
-                </p>
-                <p>
-                    No-shows, missed departures or unused services will be
-                    non-refundable. Possession of prohibited substances or drug
-                    and alcohol impairment will result in cancellation without
-                    refund. Exceptions: Group bookings of 9 or more guests and
-                    private/custom bookings have until 7-days prior their first
-                    scheduled activity to cancel and request a full refund. Any
-                    cancellations or requests received within 7- days will be
-                    considered at the discretion of Epic Adventure Group in
-                    consultation with the operator. Operator Cancellations Tour
-                    operators and/or guides have the final say on whether an
-                    activity or tour will go ahead at any given time. Such
-                    decisions may include (but are not limited to) reasons of
-                    inclement or unfavourable weather, environmental concerns,
-                    customer safety concerns or other unforeseen events that
-                    necessitate the cancellation. In such cases, Epic Adventure
-                    Group will first provide options for the customer to
-                    reschedule and then refund the cost of that activity or tour
-                    to the purchaser if rescheduling is not conducive.
-                </p>
-            </PolicyStyles>
-            <FooterDividerStyles>
-                <img
-                    src="/svg/divider-top2.svg"
-                    width="101%"
-                    style={{ marginLeft: "-.1rem" }}
-                />
-            </FooterDividerStyles>
-        </>
-    )
-}
+  return (
+    <>
+      <Head>
+        <title>Epic Adventures | Cancellation Policy</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <PolicyStyles>
+        <h3>Cancellation Policy</h3>
+        <p>
+          Purchasers have until 48-hours prior to their scheduled activity or
+          tour to cancel and request a full refund; minus $10 per item/person to
+          cover credit cards transaction fees for any items $200 or less. Any
+          refund over $200 per item/person will be calculated at 6% to cover the
+          two transactions for cancelation. Just call us at least 48 hours prior
+          to your tour to cancel to receive a refund, no questions asked.
+          Purchasers of combos or packages have until 72-hours prior to the
+          first scheduled activity to cancel and request a refund.
+        </p>
+        <p>
+          Any cancellations or requests received within 48 hours of your
+          activity or tour, late-comers, and no-shows will not be rescheduled or
+          refunded. Tour operators and/or guides have the final say on whether
+          an activity or tour will go ahead at any given time. Such decisions
+          may include (but are not limited to) reasons of inclement or
+          unfavourable weather, environmental concerns, customer safety concerns
+          or other unforeseen events that necessitate the cancellation. In such
+          cases, Epic Adventure Group will first provide options for the
+          customer to reschedule and then refund the cost of that activity or
+          tour to the purchaser if rescheduling is not conducive. Exceptions:
+          Group bookings of 9 or more guests and private/custom bookings have
+          until 7-days prior their first scheduled activity to cancel and
+          request a full refund. Any cancellations or requests received within
+          7-days will be considered at the discretion of Epic Adventure Group in
+          consultation with the operator. Possession of prohibited substances or
+          drug and alcohol impairment will result in cancellation without
+          refund.
+        </p>
+      </PolicyStyles>
+      <FooterDividerStyles>
+        <img
+          src="/svg/divider-top2.svg"
+          width="101%"
+          style={{ marginLeft: "-.1rem" }}
+        />
+      </FooterDividerStyles>
+    </>
+  );
+};
 
-export default Cancellations
+export default Cancellations;
--- pages/terms.js
+++ pages/terms.js
@@ -1,65 +1,63 @@
-import Head from "next/head"
-import { PolicyStyles } from "../components/styles/PolicyStyles"
-import { FooterDividerStyles } from "../components/styles/FooterDividerStyles"
+import Head from "next/head";
+import { PolicyStyles } from "../components/styles/PolicyStyles";
+import { FooterDividerStyles } from "../components/styles/FooterDividerStyles";
 
 const Terms = () => {
-    return (
-        <>
-            <Head>
-                <title>Epic Adventures | Terms & Conditions</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <PolicyStyles>
-                <h3>Terms & Conditions</h3>
-                <p>
-                    Full payment or deposits will be made by credit card to make
-                    a reservation. Prices are per person in Canadian Dollars,
-                    unless otherwise specified. Prices exclude GST and local
-                    taxes or fees unless otherwise stated. Price quotations are
-                    subject to change without notice until a booking has been
-                    confirmed. Tips and gratuities to tour guides are not
-                    included, but are appreciated. Certain products may involve
-                    other charges including river fees or attraction entry.
-                    Duration of tours may vary due to weather, traffic, road
-                    maintenance, and size of the group(s). While Epic Adventure
-                    Group will endeavour to co-ordinate the tour as indicated,
-                    all tours are subject to change without notice. We are not
-                    responsible for damaged personal items on tours, rides or
-                    activities. Epic Adventure Group cannot be held liable for
-                    any injuries, losses or damages in any such way. You accept
-                    the tour or activities minimum personal requirements and
-                    commit to adhere to specific safety protocols.
-                </p>
-                <p>
-                    Unless otherwise stated in the product detail, these
-                    policies apply to all products sold on this site
-                </p>
-                <h3>Covid-19 Protocols</h3>
-                <p>
-                    Anyone displaying symptoms of COVID-19, which primarily
-                    displays as a persistent cough or other typical symptoms,
-                    please stay home. If you have travelled outside Canada, you
-                    are required to self-isolate for a minimum of 14 days after
-                    arrival. If you live in a household where someone is showing
-                    symptoms of COVID-19, please stay home. If you have
-                    underlying medical conditions, it is recommended that you
-                    stay home at this time. Physical distancing is required at
-                    all times (minimum of 2 meters). Most tours and activities
-                    require a face mask and may have additional COVID-19 safety
-                    protocols that must be followed by all participants. Tours
-                    may be cancelled at any time at the discretion of the guide
-                    or tour operator.
-                </p>
-            </PolicyStyles>
-            <FooterDividerStyles>
-                <img
-                    src="/svg/divider-top2.svg"
-                    width="101%"
-                    style={{ marginLeft: "-.1rem" }}
-                />
-            </FooterDividerStyles>
-        </>
-    )
-}
+  return (
+    <>
+      <Head>
+        <title>Epic Adventures | Terms & Conditions</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <PolicyStyles>
+        <h3>Terms & Conditions</h3>
+        <p>
+          Full payment or deposits will be made by credit card to make a
+          reservation. Prices are per person in Canadian Dollars, unless
+          otherwise specified. Prices exclude GST and local taxes or fees unless
+          otherwise stated. Price quotations are subject to change without
+          notice until a booking has been confirmed. Tips and gratuities to tour
+          guides are not included, but appreciated. Certain products may involve
+          other charges including river fees or attraction entry. No shows,
+          missed departures or unused services will be non-refundable. Unless
+          otherwise stated in the product detail, these policies apply to all
+          products sold on this site. Duration of tours may vary due to weather,
+          traffic, road maintenance, and size of the group(s). While Epic
+          Adventure Group will endeavour to coordinate the tour as indicated,
+          all tours are subject to change without notice. We are not responsible
+          for damaged personal items on tours, rides or activities. Epic
+          Adventure Group cannot be held liable for any injuries, losses or
+          damages in any such way. You accept the tour or activities minimum
+          personal requirements and adhere to specific safety protocols.
+        </p>
+        <p>
+          Unless otherwise stated in the product detail, these policies apply to
+          all products sold on this site
+        </p>
+        <h3>Covid-19 Protocols</h3>
+        <p>
+          Anyone displaying symptoms of COVID-19, which primarily displays as a
+          persistent cough or other typical symptoms, please stay home. If you
+          have travelled outside Canada, you are required to self-isolate for a
+          minimum of 14 days after arrival. If you live in a household where
+          someone is showing symptoms of COVID-19, please stay home. If you have
+          underlying medical conditions, it is recommended that you stay home at
+          this time. Physical distancing is required at all times (minimum of 2
+          meters). Most tours and activities require a face mask and may have
+          additional COVID-19 safety protocols that must be followed by all
+          participants. Tours may be cancelled at any time at the discretion of
+          the guide or tour operator.
+        </p>
+      </PolicyStyles>
+      <FooterDividerStyles>
+        <img
+          src="/svg/divider-top2.svg"
+          width="101%"
+          style={{ marginLeft: "-.1rem" }}
+        />
+      </FooterDividerStyles>
+    </>
+  );
+};
 
-export default Terms
+export default Terms;
